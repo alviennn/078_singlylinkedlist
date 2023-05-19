7764b33bfdba4b969f994ab2cfd19783d5e47b58
alviennn <alvien.rnp@gmail.com>
2023-05-19 15:59:51 +0700

    untuk memeriksa apakah node yang dimaksud ada di dalam list atau tidak

--- singlelinkedlist/singlelinkedlist.cpp
+++ singlelinkedlist/singlelinkedlist.cpp
@@ -81,3 +81,15 @@ bool List::delNode(int nim)/*Mengahapus node dari dalam list*/
 	return true;
 }
 
+/*check apakah node yang dimaksud ada didalam list atau tidak*/
+bool List::search(int nim, Node** previous, Node** current) {
+	*previous = START;
+	*current = START;
+	while ((*current != NULL) && (nim != (*current)->noMhs)) 
+	{
+		*previous = *current;
+		*current = (*current)->next;
+	}
+	return (*current != NULL);
+}
+
